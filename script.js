3ddd7f276124bfcf45b052d6e3017f249c341799
Naman <37325380+nmn1406@users.noreply.github.com>
2021-11-25 12:26:18 +0530

    V1.1
    Bug Fixes and additional new features.

--- script.js
+++ script.js
@@ -6,20 +6,78 @@ function playAudio(){
          //function that clear the display
 
 function clr(){
-    document.getElementById('input').value = "";
+    document.getElementById('input').value = "0";
 }
 
 function dis(val)
          {
-             document.getElementById("input").value+=val
+             var x = document.getElementById("input").value;
+             if(x==="0"||x=="Error"){
+                x='';
+             }
+             if(disableOperator(x,val)==true){
+                x = x.slice(0,-1)
+                document.getElementById("input").value = x + val;
+             }
+             else{
+                document.getElementById("input").value = x + val;
+             }
+            //  if(x==0 && val=="/"){
+            //     // console.log("error");
+            //         x='0';
+            //         val='';
+            //  }
+                
+             
          }
+//function to disable operations if a operator is present in the end of a expression;
+
+function disableOperator(x,val){
+    let y = x.slice(-1)
+    if((y=="+" || y=="-" || y=="*" || y=="/" || y==".")&&(val=="+" || val=="-" || val=="*" || val=="/" || val==".")){
+        return true;
+    }
+}
            
-         //function that evaluates the digit and return result
+//function to evaluate the expression and return result
          
 function solve()
 {
     let x = document.getElementById("input").value
-    let y = eval(x)
-    document.getElementById("input").value = y
+    x = trimOperator(x);
+    try{
+        // eval(x);
+        let y = eval(x)
+    if(y===Infinity || isNaN(y)){
+        y = "Error";
+    }
+        document.getElementById("input").value = y;
+    }
+    catch(err){
+        document.getElementById('input').value = "Error";
+    }
+    
+        
 }
-           
+
+//function to trim a  operator if present in the end of a expression;
+
+function trimOperator(x){
+    let y = x.slice(-1)
+    if(y=="+" || y=="-" || y=="*" || y=="/" || y=="%"){
+        x = x.slice(0,-1)
+        
+    }
+    return x;
+}
+
+function backspace(){
+    let x = document.getElementById("input").value;
+    if(x.length==1 || x=="Error"){
+        x = "0";
+    }
+    else{
+        x = x.slice(0,-1);
+    }
+    document.getElementById("input").value = x;
+}
